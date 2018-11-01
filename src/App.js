0a3ab5952738a6bba71d71d9c2a344068e93b576
Joy <joy.js.mun@gmail.com>
2018-11-01 19:26:31 +0800

    finished styling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PageTemplate from './PageTemplate';
+
+class App extends Component {
+  render() {
+    return (
+     <PageTemplate>
+
+      </PageTemplate>
+    );
+  }
+}
+
+export default App;
